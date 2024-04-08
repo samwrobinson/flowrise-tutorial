6bd6d4fb5262d907172f5729d48f36ab5426527d
Sam Robinson <samrobinson@Sams-Mac-mini.home>
2024-04-08 08:47:40 -0400

    Continued styling heading and timeline components

--- src/slices/Timeline/index.tsx
+++ src/slices/Timeline/index.tsx
@@ -25,23 +25,25 @@ export type TimelineProps = SliceComponentProps<Content.TimelineSlice>;
  */
 const Timeline = ({ slice }: TimelineProps): JSX.Element => {
   return (
-    <div className="grid md:grid-cols-2 w-full h-full md:h-1/2"
+    <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      <div className="relative w-full h-full md:border-r-8 border-slate-500 max-w-screen-lg">
-      <div className="md:absolute md:left-full md:top-1/2 md:h-0.5 md:w-1/2 md:bg-slate-500 md:-z-40"></div>
-      </div>
+      <div className="grid h-1/4 md:grid-cols-2">
+        <div className="relative w-auto h-auto md:border-r-8 border-slate-500 max-w-screen-lg">
+        <div className="md:absolute md:left-full md:top-1/2 md:h-0.5 md:w-1/2 md:bg-slate-500 md:-z-40"></div>
+        </div>
 
-      <div className="m-10 md:m-28">
-      
-        <div className="z-50">
-          <PrismicNextImage className="w-full h-1/2 object-cover rounded-lg shadow-xl" field={slice.primary.event_photo} />
-          <>{slice.primary.event_title}</>
-          <PrismicRichText field={slice.primary.event_description} />
+        <div className="m-10 md:m-28">
+        
+          <div>
+            <PrismicNextImage className="w-full h-1/2 rounded-lg shadow-xl" field={slice.primary.event_photo} />
+            <>{slice.primary.event_title}</>
+            <PrismicRichText field={slice.primary.event_description} />
+          </div>
         </div>
       </div>
-    </div>
+    </Bounded>
   );
 };
 
